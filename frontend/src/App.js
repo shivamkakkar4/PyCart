1a9b97a9c24dae00e4c2cfa7d53b891167e02d17
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-02-23 00:06:23 +0530

    Initialized Header and Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <div>
-      <h1>My App</h1>
+      <Header />
+      <main>
+        <h1>Welcome</h1>
+      </main>
+      <Footer />
     </div>
   );
 };
