7254e9781cee36a66b20d60f4eee3f91947044c1
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-02-24 23:41:32 +0530

    Add and Remove from Shopping Cart Completed

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -15,5 +15,15 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       qty,
     },
   });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = id => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
