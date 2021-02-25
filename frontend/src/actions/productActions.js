599ca73f1e4aeae68a86e6c72d7056eddc177080
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-02-25 20:08:43 +0530

    User Login Redux Setup

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -22,7 +22,7 @@ export const listProducts = () => async dispatch => {
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload:
-        error.response && error.response.data.message ? error.response.data.message : error.message,
+        error.response && error.response.data.detail ? error.response.data.detail : error.message,
     });
   }
 };
@@ -41,7 +41,7 @@ export const listProductDetails = id => async dispatch => {
     dispatch({
       type: PRODUCT_DETAILS_FAIL,
       payload:
-        error.response && error.response.data.message ? error.response.data.message : error.message,
+        error.response && error.response.data.detail ? error.response.data.detail : error.message,
     });
   }
 };
